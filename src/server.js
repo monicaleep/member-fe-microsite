b95aca83a33d8cf7f6969158b50c78cdeb70ab6a
Mike Joyce <mike.joyce@shipt.com>
2024-06-20 11:15:21 -0400

    initial setup

---
+++ src/server.js
@@ -0,0 +1,61 @@
+import express from "express";
+import handlebars from "handlebars";
+import fs from "fs";
+
+const app = express();
+
+const baseDir = "./src/";
+function registerPartials() {
+  const partialDir = `${baseDir}partials`;
+  const partialFiles = fs.readdirSync(partialDir);
+  partialFiles
+    .filter((file) => file.endsWith(".html"))
+    .forEach((file) => {
+      const partialName = file.split(".")[0];
+      console.log(`Registering Partial ${partialName}`);
+      const partialHtml = fs.readFileSync(`${partialDir}/${file}`, {
+        encoding: "utf8",
+      });
+      handlebars.registerPartial(partialName, partialHtml);
+    });
+}
+
+function compileTemplates() {
+  const templateFiles = fs.readdirSync(`${baseDir}templates`);
+  // should I change all handlebars files to ebs?
+  const templateMap = {};
+  templateFiles
+    .filter((template) => template.endsWith(".html"))
+    .forEach((template) => {
+      // compile each template, store in map with key being filename
+      const filePath = `${baseDir}templates/${template}`;
+      const html = fs.readFileSync(filePath, { encoding: "utf8" });
+      const templateName = template.split(".")[0];
+      console.log(`Compiling template ${templateName}`);
+      const compiledTemplate = handlebars.compile(html);
+      templateMap[templateName] = compiledTemplate;
+    });
+  return templateMap;
+}
+const templateMap = compileTemplates();
+registerPartials();
+function render(templateName, data) {
+  const template = templateMap[templateName];
+  const html = template(data);
+  return html;
+}
+
+app.use((req, _, next) => {
+  console.log(`request for ${req.path}`);
+
+  next();
+});
+
+app.get("/", (_, res) => {
+  const html = render("index", { test: "hi there" });
+  res.status(200).send(html);
+});
+
+app.listen(3000, () => {
+  console.log("app listening on 3000");
+});
