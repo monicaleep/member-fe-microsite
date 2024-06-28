8f98d3bab45756f8c7cf2e38cfcf592ed71c0dd4
Mike Joyce <mike.joyce@shipt.com>
2024-06-28 10:55:49 -0400

    add concept of favoriting

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 import express from "express";
-import {engine} from "express-handlebars"
+import { engine } from "express-handlebars";
 import presentations from "./db/presentations.json" with { type: "json" };
 import searchPresentations from "./utils/search.js";
-import * as path from "node:path"
-import {fileURLToPath} from "node:url"
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-app.engine(".hbs", engine({extname: ".hbs"}))
-app.set("view engine", ".hbs")
-app.set("views",path.resolve(__dirname,"./views"))
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.resolve(__dirname, "./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +24,11 @@ app.get("/", (_, res) => {
 });
 
 app.get("/presentations", (_, res) => {
-  res.render("presentations", { presentations });
+  const presentationsWithFavorites = presentations.map((p) => ({
+    ...p,
+    favorited: favorites.includes(p._id),
+  }));
+  res.render("presentations", { presentations: presentationsWithFavorites });
 });
 
 app.get("/presentations/:id", (req, res) => {
@@ -33,13 +37,16 @@ app.get("/presentations/:id", (req, res) => {
     (presentation) => presentation._id == presID,
   );
   if (presentation == null) {
-    res.render('404', {message:"Presentation not found"})
-    return
+    res.render("404", { message: "Presentation not found" });
+    return;
   }
   res.render("presentation_description", {
     presentation: presentation,
   });
 });
+
+// temporary to hold favorite state in server state until we get viewstate in place
+const favorites = [ "2c3e35bd-5b08-4cd5-a537-6f056edd0a4e"];
 
 app.post("/search", (req, res) => {
   const presentationsFound = searchPresentations(
@@ -47,8 +54,9 @@ app.post("/search", (req, res) => {
     presentations,
   );
 
-  res.render("search", { presentations: presentationsFound , layout: false});
+  res.render("search", { presentations: presentationsFound, layout: false });
 });
+
 
 app.listen(3000, () => {
   console.log("app listening on 3000");
