763627dcd534588ce7e322ee4a290df2c1b9a135
Mike Joyce <mike.joyce@shipt.com>
2024-06-28 06:39:19 -0400

    cleanup comments

--- src/server.js
+++ src/server.js
@@ -11,46 +11,6 @@ app.engine(".hbs", engine({extname: ".hbs"}))
 app.set("view engine", ".hbs")
 app.set("views",path.resolve(__dirname,"./views"))
 
-// const baseDir = "./src/";
-// function registerPartials() {
-//   const partialDir = `${baseDir}partials`;
-//   const partialFiles = fs.readdirSync(partialDir);
-//   partialFiles
-//     .filter((file) => file.endsWith(".hbs"))
-//     .forEach((file) => {
-//       const partialName = file.split(".")[0];
-//       console.log(`Registering Partial ${partialName}`);
-//       const partialHtml = fs.readFileSync(`${partialDir}/${file}`, {
-//         encoding: "utf8",
-//       });
-//       handlebars.registerPartial(partialName, partialHtml);
-//     });
-// }
-
-// function compileTemplates() {
-//   const templateFiles = fs.readdirSync(`${baseDir}templates`);
-
-//   const templateMap = {};
-//   templateFiles
-//     .filter((template) => template.endsWith(".hbs"))
-//     .forEach((template) => {
-//       // compile each template, store in map with key being filename
-//       const filePath = `${baseDir}templates/${template}`;
-//       const html = fs.readFileSync(filePath, { encoding: "utf8" });
-//       const templateName = template.split(".")[0];
-//       console.log(`Compiling template ${templateName}`);
-//       const compiledTemplate = handlebars.compile(html);
-//       templateMap[templateName] = compiledTemplate;
-//     });
-//   return templateMap;
-// }
-// const templateMap = compileTemplates();
-// registerPartials();
-// function render(templateName, data) {
-//   const template = templateMap[templateName];
-//   const html = template(data);
-//   return html;
-// }
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, _, next) => {
@@ -68,7 +28,6 @@ app.get("/presentations", (_, res) => {
 });
 
 app.get("/presentations/:id", (req, res) => {
-  //the id path
   const presID = req.params.id;
   const presentation = presentations.find(
     (presentation) => presentation._id == presID,
