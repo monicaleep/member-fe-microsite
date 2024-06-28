001526dc83665983fa340bc0dbe8713dc797b610
Mike Joyce <mike.joyce@shipt.com>
2024-06-28 05:52:46 -0400

    add more tests

--- cypress/e2e/presentations.cy.js
+++ cypress/e2e/presentations.cy.js
@@ -14,4 +14,24 @@ describe("Presentations Page", () => {
     cy.getByTestId("presentation-search").type("gmail");
     cy.getByTestId("presentation-row").should("have.length", 6);
   });
+
+  it("should link to presentation detail page", () => {
+    cy.visit("/presentations");
+
+    cy.getByTestId("presentation-search").type("transform customized");
+    cy.getByTestId("presentation-row").should("have.length", 1);
+    cy.get("a").click();
+    cy.url().should(
+      "include",
+      "/presentations/7415a027-865c-4112-aff4-f617cc3093d2",
+    );
+    cy.contains("h1", "transform customized e-markets")
+  });
+
+  it("should show 404 if linking to unknown presentation page", () => {
+
+    cy.visit("/presentations/foo", {failOnStatusCode: false});
+    cy.contains("h1", "Presentation not found")
+
+  })
 });
