b828fb9d6d4c5d88d8901af0961223e8b90fe1e2
Mike Joyce <mike.joyce@shipt.com>
2024-06-28 06:20:06 -0400

    configure server to use view engine

--- src/server.js
+++ src/server.js
@@ -1,51 +1,56 @@
 import express from "express";
-import handlebars from "handlebars";
-import fs from "fs";
+import {engine} from "express-handlebars"
 import presentations from "./db/presentations.json" with { type: "json" };
 import searchPresentations from "./utils/search.js";
+import * as path from "node:path"
+import {fileURLToPath} from "node:url"
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const app = express();
+app.engine(".hbs", engine({extname: ".hbs"}))
+app.set("view engine", ".hbs")
+app.set("views",path.resolve(__dirname,"./views"))
 
-const baseDir = "./src/";
-function registerPartials() {
-  const partialDir = `${baseDir}partials`;
-  const partialFiles = fs.readdirSync(partialDir);
-  partialFiles
-    .filter((file) => file.endsWith(".hbs"))
-    .forEach((file) => {
-      const partialName = file.split(".")[0];
-      console.log(`Registering Partial ${partialName}`);
-      const partialHtml = fs.readFileSync(`${partialDir}/${file}`, {
-        encoding: "utf8",
-      });
-      handlebars.registerPartial(partialName, partialHtml);
-    });
-}
+// const baseDir = "./src/";
+// function registerPartials() {
+//   const partialDir = `${baseDir}partials`;
+//   const partialFiles = fs.readdirSync(partialDir);
+//   partialFiles
+//     .filter((file) => file.endsWith(".hbs"))
+//     .forEach((file) => {
+//       const partialName = file.split(".")[0];
+//       console.log(`Registering Partial ${partialName}`);
+//       const partialHtml = fs.readFileSync(`${partialDir}/${file}`, {
+//         encoding: "utf8",
+//       });
+//       handlebars.registerPartial(partialName, partialHtml);
+//     });
+// }
 
-function compileTemplates() {
-  const templateFiles = fs.readdirSync(`${baseDir}templates`);
+// function compileTemplates() {
+//   const templateFiles = fs.readdirSync(`${baseDir}templates`);
 
-  const templateMap = {};
-  templateFiles
-    .filter((template) => template.endsWith(".hbs"))
-    .forEach((template) => {
-      // compile each template, store in map with key being filename
-      const filePath = `${baseDir}templates/${template}`;
-      const html = fs.readFileSync(filePath, { encoding: "utf8" });
-      const templateName = template.split(".")[0];
-      console.log(`Compiling template ${templateName}`);
-      const compiledTemplate = handlebars.compile(html);
-      templateMap[templateName] = compiledTemplate;
-    });
-  return templateMap;
-}
-const templateMap = compileTemplates();
-registerPartials();
-function render(templateName, data) {
-  const template = templateMap[templateName];
-  const html = template(data);
-  return html;
-}
+//   const templateMap = {};
+//   templateFiles
+//     .filter((template) => template.endsWith(".hbs"))
+//     .forEach((template) => {
+//       // compile each template, store in map with key being filename
+//       const filePath = `${baseDir}templates/${template}`;
+//       const html = fs.readFileSync(filePath, { encoding: "utf8" });
+//       const templateName = template.split(".")[0];
+//       console.log(`Compiling template ${templateName}`);
+//       const compiledTemplate = handlebars.compile(html);
+//       templateMap[templateName] = compiledTemplate;
+//     });
+//   return templateMap;
+// }
+// const templateMap = compileTemplates();
+// registerPartials();
+// function render(templateName, data) {
+//   const template = templateMap[templateName];
+//   const html = template(data);
+//   return html;
+// }
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, _, next) => {
@@ -55,8 +60,7 @@ app.use((req, _, next) => {
 });
 
 app.get("/", (_, res) => {
-  const html = render("index", { test: "hi there" });
-  res.status(200).send(html);
+  res.render("index", { test: "hi there" });
 });
 
 app.get("/presentations", (_, res) => {
