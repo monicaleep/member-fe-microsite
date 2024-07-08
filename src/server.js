e78733b723f5a2bb68a8456d95962471033ef69c
Mike Joyce <mikejoyce19@gmail.com>
2024-07-08 09:29:49 -0400

    [feat]: Presentation Favoriting Phase 1 (#9)
    * add concept of favoriting
    
    * wire up favoriting!
    
    * change favorited_cell to use span instead of td, use emojis
    
    * wrap in td
    
    * add favorited button to presentation_description
    
    * clear favorites
    
    * add test for favoriting
    
    * update header
    
    * add another test
    
    * update route to match pluralization of other routes
    
    * add more tests
    
    * add cookie-session
    
    * integrate cookie session
    
    * fix bug with search not respecting favorites
    
    * fix test as state isnt shared anymore
    
    * DRY up selectors
    
    * write up dem tests
    
    * create util method for joining presentations with favorites
    
    * write test asserting search and favoriting works
    
    * formatting?
    
    * nodemon ignore cypress directory
    
    * fix merge error

--- src/server.js
+++ src/server.js
@@ -1,58 +1,111 @@
 import express from "express";
-import {engine} from "express-handlebars"
+import { engine } from "express-handlebars";
+import cookieSession from "cookie-session";
 import presentations from "./db/presentations.json" with { type: "json" };
 import searchPresentations from "./utils/search.js";
-import * as path from "node:path"
-import {fileURLToPath} from "node:url"
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+import { mergePresentationsWithFavorites } from "./utils/favorites.js";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-app.engine(".hbs", engine({extname: ".hbs"}))
-app.set("view engine", ".hbs")
-app.set("views",path.resolve(__dirname,"./views"))
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.resolve(__dirname, "./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1"],
+    httpOnly: false,
+    signed: false,
+    maxAge: 365 * 24 * 60 * 60 * 1000, // one year
+  }),
+);
 app.use((req, _, next) => {
   console.log(`request for ${req.path}`);
+  next();
+});
+
+app.use((req, _, next) => {
+  let favoritePresentations = req.session.favorite_presentations;
+  if (favoritePresentations == null) {
+    req.session.favorite_presentations = [];
+  }
 
   next();
 });
-app.use("/public", express.static(path.resolve(__dirname, 'public')));
+app.use("/public", express.static(path.resolve(__dirname, "public")));
 
 app.get("/", (_, res) => {
   res.render("index", { test: "hi there" });
 });
 
-app.get("/presentations", (_, res) => {
+app.get("/presentations", (req, res) => {
+  const favorites = req.session.favorite_presentations;
+  const presentationsWithFavorites = mergePresentationsWithFavorites(
+    presentations,
+    favorites,
+  );
+
   res.render("presentations", {
     title: "Presentations",
-    presentations,
+    presentations: presentationsWithFavorites,
   });
 });
 
 app.get("/presentations/:id", (req, res) => {
   const presID = req.params.id;
+  const favorites = req.session.favorite_presentations;
   const presentation = presentations.find(
     (presentation) => presentation._id == presID,
   );
   if (presentation == null) {
-    res.render('404', {message:"Presentation not found"})
-    return
+    res.render("404", { message: "Presentation not found" });
+    return;
   }
+  const favorited = favorites.includes(presentation._id);
   res.render("presentation_description", {
     title: presentation.name,
-    presentation: presentation,
+    presentation: { ...presentation, favorited },
   });
 });
 
 app.post("/search", (req, res) => {
+  const favorites = req.session.favorite_presentations;
   const presentationsFound = searchPresentations(
     req.body.search,
     presentations,
   );
+  const presentationsWithFavorites = mergePresentationsWithFavorites(
+    presentationsFound,
+    favorites,
+  );
+  res.render("search", {
+    presentations: presentationsWithFavorites,
+    layout: false,
+  });
+});
 
-  res.render("search", { presentations: presentationsFound , layout: false});
+app.put("/presentations/favorite/:id", (req, res) => {
+  const id = req.params.id;
+  let favorites = req.session.favorite_presentations;
+  // what if id doesn't exist???
+
+  // we'll toggle favorite on and off with this endpoint
+  const isExistingFavorite = favorites.includes(id);
+  favorites = isExistingFavorite
+    ? favorites.filter((f) => f !== id)
+    : [...favorites, id];
+
+  req.session.favorite_presentations = favorites;
+  res.render("favorited_response", {
+    favorited: !isExistingFavorite,
+    id,
+    layout: false,
+  });
 });
 
 app.listen(3000, () => {
---
+++ src/utils/favorites.js
@@ -0,0 +1,6 @@
+export function mergePresentationsWithFavorites(presentations, favoriteIds){
+  return presentations.map((p) => ({
+    ...p,
+    favorited: favoriteIds.includes(p._id),
+  }));
+}
