4bc8f885a37cc863686527c685039245a315d332
Mike Joyce <mike.joyce@shipt.com>
2024-07-11 10:57:10 -0400

    remove lastName

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,6 +1,6 @@
 const searchPresentations = (query, presentations) => {
   return presentations.filter((pres) => {
-    const searchableFields = [pres.name, pres.firstName, pres.lastName];
+    const searchableFields = [pres.name, pres.firstName];
     return searchableFields.some((field) =>
       field.toLowerCase().includes(query.toLowerCase()),
     );
